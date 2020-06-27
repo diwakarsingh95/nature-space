7d494db43c8168cedaaedf65ac2844db9b295bbe
Diwakar <42492536+diwakarsingh95@users.noreply.github.com>
2020-06-27 18:56:29 +0530

    updated top bar and yoga quotes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import { Grid, Paper, Box } from '@material-ui/core'
-import styles from './Home.module.css'
-import Navbar from '../Navbar/Navbar'
-import Footer from '../Footer/Footer'
-import Square1 from '../../assets/images/square-1.jpg'
-import Square2 from '../../assets/images/square-2.jpg'
-import Square3 from '../../assets/images/square-3.jpg'
+import React from 'react';
+import { Grid, Paper, Box } from '@material-ui/core';
+import styles from './Home.module.css';
+import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer/Footer';
+import Square1 from '../../assets/images/square-1.jpg';
+import Square2 from '../../assets/images/square-2.jpg';
+import Square3 from '../../assets/images/square-3.jpg';
+import AllInclusiveIcon from '@material-ui/icons/AllInclusive';
 
 const Home = () => {
   return (
@@ -13,14 +14,19 @@ const Home = () => {
       <Navbar />
       <div className={styles.Layout}>
         <Paper className={styles.paper} elevation={4}>
-          <Grid container justify="flex-start">
+          <Grid container justify='flex-start'>
             <Box clone order={{ xs: 2, sm: 1 }}>
               <Grid item xs={12} sm={7} md={8}>
                 <div className={styles.paperText}>
-                  <h2>Yoga related quote 1.</h2>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  </p>
+                  <blockquote>
+                    ”Yoga is not just repetition of few postures, it is more
+                    about the exploration and discovery of the subtle energies
+                    of life”
+                  </blockquote>
+                  <p>― Amit Ray</p>
+                  <div className={styles.endIcon}>
+                    <AllInclusiveIcon fontSize='large' />
+                  </div>
                 </div>
               </Grid>
             </Box>
@@ -38,10 +44,15 @@ const Home = () => {
             </Grid>
             <Grid item xs={12} sm={7} md={8}>
               <div className={styles.paperText}>
-                <h2>Yoga related quote 2.</h2>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
+                <blockquote>
+                  "When you listen to yourself, everything comes naturally. It
+                  comes from inside, like a kind of will to do something. Try to
+                  be sensitive. That is yoga."
+                </blockquote>
+                <p>― Petri Räisänen</p>
+                <div className={styles.endIcon}>
+                  <AllInclusiveIcon fontSize='large' />
+                </div>
               </div>
             </Grid>
           </Grid>
@@ -51,10 +62,14 @@ const Home = () => {
             <Box clone order={{ xs: 2, sm: 1 }}>
               <Grid item xs={12} sm={7} md={8}>
                 <div className={styles.paperText}>
-                  <h2>Yoga related quote 3.</h2>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  </p>
+                  <blockquote>
+                    "The very heart of yoga practice is ‘abyhasa’ steady effort
+                    in the direction you want to go."
+                  </blockquote>
+                  <p>― Sally Kempton</p>
+                  <div className={styles.endIcon}>
+                    <AllInclusiveIcon fontSize='large' />
+                  </div>
                 </div>
               </Grid>
             </Box>
@@ -68,7 +83,7 @@ const Home = () => {
       </div>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
